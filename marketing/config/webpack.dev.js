5fa8b6333a50886b6f3797ce4afe97c1b8d36059
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-01-12 18:08:16 +0530

    ADD: webpack dev config & merge common and dev config

---
+++ marketing/config/webpack.dev.js
@@ -0,0 +1,21 @@
+// merge is a function used to merge two webpack config objects
+const {merge} = require("webpack-merge")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+    mode: "development",
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: "index.html"
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
