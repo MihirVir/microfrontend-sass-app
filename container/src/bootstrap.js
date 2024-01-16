2dcfc207fe27127e2200b5c2b294811e2521e14b
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-01-16 15:24:14 +0530

    adding comments

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+// rendering the container id
 ReactDOM.render(
     <App />,
     document.querySelector("#root")
