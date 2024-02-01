d5f5359e9fa210eb05d09024881d400c15f35e51
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-02-01 21:49:52 +0000

    marketing re run

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,3 +1,4 @@
+// marketing prod file 
 const {merge} = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
