6df6966047d2b2ba14be3236e8fdea3a18758191
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-02-02 09:06:03 +0000

    Minor changes

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./App.js";
 // Mount function to start up the app 
 const mount = (el) => {
     ReactDOM.render(
